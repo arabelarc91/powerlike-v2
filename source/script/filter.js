81d9757bbc0d2c7fd8e4eb9ba96c1ec5729d5a67
arabela <arabela.rojas@srburns.pe>
2019-12-03 18:50:57 -0500

    Vista de ahorrador

--- source/script/filter.js
+++ source/script/filter.js
@@ -4,15 +4,23 @@ $(function() {
     var selectProfile = (function() {
         var inputInitial = {
             slider_options: ".powerlike_options__carousel",
-            select_icon :".select-icon"
+            select_icon :".select-icon",
+            counter_button: ".counter"
         }
 
         var suscribeEvents = function() {
             var selectIcon = $(inputInitial.select_icon)
             selectIcon.on("click", events.list_brands)
+
+            var buttonCounter = $(inputInitial.counter_button)
+            buttonCounter.on("click", events.counter_button)
         }
         
         var events = {
+            counter_button: function () {
+                $(this).toggleClass("active")
+            },
+
             list_brands: function () {
                 $(this).toggleClass("active")
             },
