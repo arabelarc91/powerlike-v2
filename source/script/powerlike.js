b9098d0317e64aa9342c3e84995a1852e207852c
willy.robles <willy.robles@srburns.pe>
2019-12-05 17:08:57 -0500

    Se inicio desarollo de flujo de navegacion

--- source/script/powerlike.js
+++ source/script/powerlike.js
@@ -1,13 +1,21 @@
+let greeting = {
+    events: function() {
+        $('.powerlike_profile_market__content__options__button').click(function() {
+            flow.setHash('/start')
+        })
+    }
+}
+
 let saver = {
-    events : function () {
-        $('.select-icon').click(function () {
-            $(this).toggleClass("active")            
+    events: function() {
+        $('.select-icon').click(function() {
+            $(this).toggleClass("active")
         })
     }
 }
 
 let sliders = {
-    profiles : function () {
+    profiles: function() {
         $('.powerlike_options__carousel').slick({
             arrows: false,
             dots: true,
@@ -19,8 +27,7 @@ let sliders = {
             centerPadding: '20px'
         })
     },
-
-    devices : function () {
+    devices: function () {
         $(".powerlike_devicedetail__content__device").slick({
             arrows: false,
             dots: true,
@@ -34,10 +41,53 @@ let sliders = {
     }
 }
 
-let initialize = function() {
+let flow = {
+    setHash: function(hash) {
+        // '/value'
+        window.location.hash = hash
+    },
+    detectPopState: function() {
+        let hashValue
+
+        window.addEventListener('popstate', function() {
+            hashValue = window.location.hash
+            flow.changeScreen(hashValue)
+        })
+    },
+    changeScreen: function(hashValue) {
+        $('.entel-powerlike section').removeClass('active')
+        
+        if (hashValue === '') {
+            $('.powerlike_profile_market').addClass('active')
+        } else if (hashValue === '#/start') {
+            $('.powerlike_options').addClass('active')
+        }
+    }
+}
+
+let load_greeting = function() {
+    greeting.events()
+}
+
+let load_saver = function() {
     saver.events()
+}
+
+let load_slider = function() {
     sliders.profiles()
     sliders.devices()
+}
+
+let load_flow = function() {
+    flow.detectPopState()
+    flow.changeScreen(window.location.hash)
+}
+
+let initialize = function() {
+    load_greeting()
+    load_saver()
+    load_slider()
+    load_flow()
 }
 
 $(document).ready(function() {
