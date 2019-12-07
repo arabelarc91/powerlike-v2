50624f23481ef1bd20155cab5a21c663a11a6a72
arabela <arabela.rojas@srburns.pe>
2019-12-06 19:09:41 -0500

    Caracteristicas

--- source/script/powerlike.js
+++ source/script/powerlike.js
@@ -5,11 +5,9 @@ let greeting = {
         })
     }
 }
-
 let profiles = {
     setValButton: function() {
         let perfilText
-
         $('.powerlike_options__carousel').on('afterChange', function(event, slick, currentSlide, nextSlide){
             perfilText =  $('.powerlike_options__carousel .slick-current .tag').text()
             $('#profile_name').text(perfilText)
@@ -17,45 +15,37 @@ let profiles = {
     },
     events: function() {
         let perfilText
-
         $('.button_entel__carousel > a').click(function() {
             perfilText =  $('.powerlike_options__carousel .slick-current .tag').text()
             flow.showPerfil(perfilText)
         })
     }
 }
-
 let profile_selected = {
     events: function() {
         let hash
-
         $('.powerlike_chooseprofile .button_entel__carousel > a').click(function() {
             hash = window.location.hash
-
             flow.setHash('/modalidad')
             $('.powerlike__preselector').attr('referer',hash.replace('#/',''))
         })
     }
 }
-
 let preselector = {
     events: function() {
-<<<<<<< HEAD
-        let modality
-
         $('.powerlike__preselector .powerlike__preselector__content__buttons').click(function() {
-            modality = $(this).attr('filter')            
-=======
-        $('.select-icon').click(function() {
-            $(this).toggleClass("active")
-            $('.powerlike_chooseprofile__select ul').toggleClass("active")
->>>>>>> 6c567d72695db34f8b0acdb58c4a76a9adcec1de
+            flow.setHash('/matches')
         })
     }
 }
-
+let matches = {
+    events: function() {
+        $('.powerlike_devicedetail .link-detalle').click(function() {
+            console.log('sadadsdsdas')
+        })
+    }
+}
 // -----------------------------
-
 let sliders = {
     profiles: function() {
         $('.powerlike_options__carousel').slick({
@@ -81,7 +71,6 @@ let sliders = {
             centerPadding: '20px'
         })
     },
-
     match_device: function() {
         $('.powerlike_devicedetail__content__match__slide').slick({
             arrows: false,
@@ -95,7 +84,14 @@ let sliders = {
         })
     }
 }
-
+let selects = {
+    events: function() {
+        $('.select-icon').click(function() {
+            $(this).toggleClass("active")
+            $('.powerlike_chooseprofile__select ul').toggleClass("active")
+        })
+    }
+}
 let flow = {
     setHash: function(hash) {
         // '/value'
@@ -103,7 +99,6 @@ let flow = {
     },
     detectPopState: function() {
         let hashValue
-
         window.addEventListener('popstate', function() {
             hashValue = window.location.hash
             flow.changeScreen(hashValue)
@@ -116,24 +111,20 @@ let flow = {
         // decisions tree
         if (hashValue === '') {
             $('.powerlike_profile_market').addClass('active')
-
         } else if (hashValue === '#/perfiles') {
             $('.powerlike_options').addClass('active')
-
         } else if (hashValue === '#/gamer') {
             $('.powerlike_chooseprofile.gamer').addClass('active')
-
         } else if (hashValue === '#/influencer') {
             $('.powerlike_chooseprofile.influencer').addClass('active')
-
         } else if (hashValue === '#/aventurero') {
             $('.powerlike_chooseprofile.aventurero').addClass('active')
-
         } else if (hashValue === '#/ahorrador') {
             $('.powerlike_chooseprofile.ahorrador').addClass('active')
-
         } else if (hashValue === '#/modalidad') {
-            $('.powerlike__preselector').addClass('active')            
+            $('.powerlike__preselector').addClass('active')
+        } else if (hashValue === '#/matches') {
+            $('.powerlike_devicedetail').addClass('active')
         }
     },
     showPerfil: function(perfilText) {
@@ -144,67 +135,65 @@ let flow = {
         
         $('.arrow-back').click(function() {
             hashValue = window.location.hash
-
             if (hashValue === '#/perfiles') {
                 flow.setHash('')
-
             } else if (hashValue === '#/gamer' || hashValue === '#/influencer' || hashValue === '#/aventurero' || hashValue === '#/ahorrador') {
                 flow.setHash('/perfiles')
-
             }  else if (hashValue === '#/modalidad') {
                 // move back to profile
                 let referer = $('.powerlike__preselector').attr('referer')
                 
-                if (referer != undefined) {
+                if (referer !== undefined) {
                     flow.setHash('/' + referer)
                 } else {
                     flow.setHash('')
-                }            
+                }
+            } else if (hashValue === '#/matches') {
+                flow.setHash('/modalidad')                
             }
         })
     }
 }
-
 // -----------------------------
-
 let load_greeting = function() {
     greeting.events()
 }
-
 let load_profiles = function() {
     profiles.setValButton()
     profiles.events()
 }
-
 let load_profile_selected = function() {
     profile_selected.events()
 }
-
 let load_preselector = function() {
     preselector.events()
 }
-
 let load_slider = function() {
     sliders.profiles()
     sliders.devices()
     sliders.match_device()
 }
-
+let load_selects = function() {
+    selects.events()
+}
 let load_flow = function() {
     flow.detectPopState()
     flow.changeScreen(window.location.hash)
     flow.backEvent()
 }
-
+let load_matches = function() {
+    matches.events()
+}
 let initialize = function() {    
     load_greeting()
     load_profiles()
     load_profile_selected()
     load_preselector()
     load_slider()
+    load_selects()
     load_flow()
+    load_matches()
 }
-
 $(document).ready(function() {
     initialize()
 })
